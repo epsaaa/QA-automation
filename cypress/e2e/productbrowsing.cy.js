594f07138184cf1015d2137e19746e8f01e9d2ae
USER <epsa.201614@ncit.edu.np>
2025-05-21 10:57:52 +0545

    verify with one keyword

--- cypress/e2e/productbrowsing.cy.js
+++ cypress/e2e/productbrowsing.cy.js
@@ -1,17 +1,25 @@
-describe('Product browsing and filtering', () => {
-    it('should navigate to product page', () => {
-        cy.visit('https://automationexercise.com/');
+describe("Product browsing and filtering", () => {
+  it("should navigate to product page", () => {
+    cy.visit("https://automationexercise.com/");
 
-        cy.contains('Products').click();
+    cy.contains("Products").click();
 
-        cy.url().should('include', '/products');
-        cy.contains('All Products').should('be.visible');
+    cy.url().should("include", "/products");
+    cy.contains("All Products").should("be.visible");
 
-        cy.contains('Women').click();
-        cy.get('.panel-body').contains('Dress').click(); 
+    cy.contains("Women").click();
+    cy.get(".panel-body").contains("Dress").click();
 
-        cy.contains('Women - Dress Products').should('be.visible');
-        
-    })
-    
-})
+    cy.contains("Women - Dress Products").should("be.visible");
+
+   cy.get('.productinfo.text-center')
+  .should('exist')
+  .each(($el) => {
+    const text = $el.text().toLowerCase();
+    if (text.includes('dress')) {
+      expect(text).to.include('dress');
+      return false; 
+    }
+  });
+      });
+  });

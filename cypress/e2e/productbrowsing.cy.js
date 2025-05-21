01322d2791e5658b1a5b84902353c844df0c8e88
USER <epsa.201614@ncit.edu.np>
2025-05-21 11:22:27 +0545

    verify product details

--- cypress/e2e/productbrowsing.cy.js
+++ cypress/e2e/productbrowsing.cy.js
@@ -12,14 +12,23 @@ describe("Product browsing and filtering", () => {
 
     cy.contains("Women - Dress Products").should("be.visible");
 
-   cy.get('.productinfo.text-center')
-  .should('exist')
-  .each(($el) => {
-    const text = $el.text().toLowerCase();
-    if (text.includes('dress')) {
-      expect(text).to.include('dress');
-      return false; 
-    }
-  });
+    cy.get(".productinfo.text-center")
+      .should("exist")
+      .each(($el) => {
+        const text = $el.text().toLowerCase();
+        if (text.includes("dress")) {
+          expect(text).to.include("dress");
+          return false;
+        }
       });
+
+    cy.contains("View Product").click();
+    cy.url().should("include", "/product_details/3");
+
+    cy.get(".product-information h2").should("be.visible");
+    cy.get(".product-information span span").first().should("be.visible");
+    cy.get(".product-information p")
+      .contains("Availability")
+      .should("be.visible");
   });
+});
